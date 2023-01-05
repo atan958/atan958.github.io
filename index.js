a5b9f3d6d71b179dd31169dbd461c2dd56812da7
Angelo Tangonan <Angelo.Tangonan@vista.co>
2023-01-06 11:03:36 +1300

    feat: testing v3

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const testElm = document.getElementById('testId');
-
 testElm.innerHTML = 'testing...';
+
+const testUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+fetch(testUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.name);
+        testElm.innerHTML = `testing...${data.name}`;
+    });
