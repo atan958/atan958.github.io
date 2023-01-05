933137ee2b7e494c9073ce8bc50b933a5daa9d03
Angelo Tangonan <Angelo.Tangonan@vista.co>
2023-01-06 12:20:35 +1300

    feat: testing v5

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const testElm = document.getElementById('testId');
-const currentIteration = 1;
+const currentIteration = 2;
 testElm.innerHTML = `testing #${currentIteration}...`;
 
 const testUrlV1 = 'https://pokeapi.co/api/v2/pokemon/ditto';
 const testUrlV2 = 'https://media-library-api.vistadevtest.workers.dev/api/test';
 
-fetch(testUrlV2)
+fetch(testUrlV2, { method: 'POST' })
     .then(res => res.text())
     .then(data => {
         testElm.innerHTML = `testing #${currentIteration}...${data}`;
