65ee0c09e4f97c16d5c6830c6bdd5e9a23054e87
Angelo Tangonan <Angelo.Tangonan@vista.co>
2023-01-07 12:37:00 +1300

    feat: testing v8

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const testElm = document.getElementById('testId');
-const currentIteration = 5;
+const currentIteration = 6;
 testElm.innerHTML = `testing #${currentIteration}...`;
 
 const testUrlV1 = 'https://pokeapi.co/api/v2/pokemon/ditto';
-const testUrlV2 = 'https://media-library-api.vistadevtest.workers.dev/api/upload/fried-chicken-v3.jpg';
-const testUrlV3 = 'https://media-library-api.vistadevtest.workers.dev/api/test';
+const testUrlV2 = 'https://media-library-api.vistadevtest.workers.dev/api/test';
 
-fetch(testUrlV3, { 
-    method: 'POST', 
-    headers: {
-        'X-Custom-Auth-Key': 'pussinboots'
-    }
-})
+fetch(testUrlV2, { method: 'POST' })
     .then(res => res.text())
     .then(data => {
         testElm.innerHTML = `testing #${currentIteration}...${data}`;
