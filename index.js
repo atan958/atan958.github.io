cf6c3a6d6a118009321b6cfe53500a70bc315ba3
Angelo Tangonan <Angelo.Tangonan@vista.co>
2023-01-07 15:13:46 +1300

    feat: testing v15

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const testElm = document.getElementById('testId');
-const currentIteration = 12;
+const currentIteration = 13;
 testElm.innerHTML = `testing #${currentIteration}...`;
 
 const testUrlV1 = 'https://pokeapi.co/api/v2/pokemon/ditto';
@@ -39,7 +39,10 @@ async function uploadFileAsBinary(file) {
 
     fetch(testUrlV2, { 
         method: 'POST',
-        body: file
+        body: binary,
+        headers: {
+            'content-type': 'image/jpg'
+        }
     })
         .then(res => res.text())
         .then(data => {
